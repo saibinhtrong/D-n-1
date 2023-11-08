ff3f18fc060333fe159a4652b2226e9b060ce02b
binhtrong4354t <saibinhtrong18122003@gmail.com>
2023-11-08 10:59:37 +0700

    demo

--- Admin/home.php
+++ Admin/home.php
@@ -1,1 +1,14 @@
-vsdffdagdf
+<main class="app-content_admin">
+    <div class="conten_admin">
+        <h4>Welcome</h4>
+        <p>
+            Xin chào Adimn, chào mừng bạn trở lại
+        </p>
+        <div class="image_homes">
+        </div>
+    </div>
+</main>
+<main class="app-content_footer">
+    <div class="conten_admin">
+    </div>
+</main>
