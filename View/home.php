bef37805a57bce033c08651636662b4807bf84bc
thaidtph44418 <149986783+thaidtph44418@users.noreply.github.com>
2023-11-10 12:41:34 +0700

    home.php

--- View/home.php
+++ View/home.php
@@ -1,1 +1,21 @@
-ihowfyoewf
+<div class="row magin fixed">
+    <div class="left magin_right">
+        <div class="row">
+            <div class="banner">
+                <div class="icons" id="pre">
+                    <i class="fa-solid fa-circle-arrow-left"></i>
+                </div>
+                <img src="images/anh0.png" alt="" id="anh">
+                <div class="iconst" id="next">
+                    <i class="fa-solid fa-circle-arrow-right"></i>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="cont_sp magin10">
+                <h3 style="color: red;">Sản phẩm</h3>
+            </div>
+        </div>
+    </div>
+    </div>
+</div>
