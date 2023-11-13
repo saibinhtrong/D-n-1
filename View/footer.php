de455c81e93cac4bb0fb9195eabd57db732a18e7
thai <damthuanthai@gmail.com>
2023-11-13 09:47:43 +0700

    home page

---
+++ View/footer.php
@@ -0,0 +1,84 @@
+<div class="row footer">
+    <div class="footer_info">
+        <div class="infor_tg">
+            <p>Công Ty TNHH Thế Giới Di Động</p>
+            <div class="conten_footer">
+                <h7>Tích điểm quà tặng VIP</h7>
+                <h7>Thông tin bảo hành</h7>
+                <h7>Điều khoản sử dụng</h7>
+            </div>
+        </div>
+        <div class="certification">
+            <div class="infor_tg">
+                <p>Tổng đài hỗ trợ miễn phí</p>
+                <h7>0876-55-2004</h7>
+            </div>
+            <div class="image_footer">
+                <div class="notin">
+                    <img src="image/logoSaleNoti.png" alt="">
+                </div>
+                <div class="dmca">
+                    <img src="image/dmca-website-logo-2022.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="sivers">
+        © 2023 TGDĐ hệ thống được phát triển và vận hành
+    </div>
+</div>
+</div>
+<aside>
+    <div class="back_top">
+        <a href="#">
+            <i class="fa-solid fa-upload"></i>
+            Lên đầu
+        </a>
+    </div>
+    <div class="float-contact_back">
+        <div id="wrapper">
+            <div class="list-button">
+                <a href="#">
+                    <label class="button-icon">
+                        <i class="fa-brands fa-facebook-messenger"></i>
+                    </label>
+                </a>
+                <span style="--i:0"></span>
+                <span style="--i:1"></span>
+                <span style="--i:2"></span>
+                <span style="--i:3"></span>
+            </div>
+        </div>
+    </div>
+    </div>
+</aside>
+<script>
+    const toTop = document.querySelector(".back_top");
+    window.addEventListener("scroll", () => {
+        if (window.pageYOffset > 100) {
+            toTop.classList.add("active");
+        } else {
+            toTop.classList.remove("active");
+        }
+    })
+</script>
+<!-- <script>
+    // Function hiển thị thông báo
+    function showWelcomeNotification() {
+        document.getElementById('welcomeNotification').style.display = 'block';
+        setTimeout(closeNotification, 2000);
+    }
+    // Function đóng thông báo
+    function closeNotification() {
+        document.getElementById('welcomeNotification').style.display = 'none';
+    }
+    // Gọi hàm hiển thị thông báo khi trang được tải
+    window.onload = function() {
+        showWelcomeNotification();
+    };
+</script> -->
+<script src="javascript/js.js"></script>
+<script src="javascript/slide.js"></script>
+<script src="javascript/loader.js"></script>
+</body>
+</html>
---
+++ View/header.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+    <link id="favicon" rel="icon" type="image/logo-acgen.png" sizes="50x50" href="image/logo-acgen.png">
+    <link rel="stylesheet" href="style/style.css">
+
+    <title>Shop Schannel</title>
+</head>
+
+<body>
+    <!-- <div class="warper">
+        <div class="loading-container">
+            <div class="loader"></div>
+
+        </div> -->
+    <div class="container">
+        <!-- thông báo load trang -->
+        <!-- <div class="notification" id="welcomeNotification">
+                <img src="https://cellphones.com.vn/smember/_nuxt/img/Shipper_CPS3.77d4065.png" alt="">
+                <h1>Xin Chào</h1>
+            </div> -->
+        <header >
+            <div class="logo">
+                <img src="image/logo-Schannel-Network-crop.png">
+            </div>
+            <form action="index.php?act=product_list" method="post">
+                <div class="search">
+                    <input name="kyw" placeholder=" Tìm Kiếm" type="text">
+                    <button type="submit" name="timkiem"> <i class="fa-solid fa-magnifying-glass"></i></button>
+                </div>
+            </form>
+            <div class="synthetic">
+                <i class="fa-sharp fa-solid fa-location-dot"></i> Địa chỉ : Trịnh Văn Bô - Thành Phố Hà Nội 
+            </div>
+            <div>
+            
+            </div>
+                <div class="users">
+                    <a href="index.php?act=login" class="active">
+                        <i class="fa-solid fa-user-shield"></i>
+                        Tài Khoản
+                    </a>
+                </div>
+        </header>
+        <div class=" menu">
+            <ul>
+                <li>
+                    <a href="index.php?act=home" class="active" style="--i:1;">Trang Chủ</a>
+                </li>
+                <li>
+                    <a href="index.php?act=about" style="--i:2">Giới Thiệu</a>
+                </li>
+                <li>
+                    <a href="index.php?act=contact" style="--i:3;">Liên Hệ</a>
+                </li>
+                <li>
+                    <a href="index.php?act=feedback" style="--i:4;">Góp ý</a>
+                </li>
+                <li>
+                    <a href="index.php?act=ask" style="--i:5;">Hỏi Đáp</a>
+                </li>
+                <li>
+                    <marquee>
+                    </marquee>
+                </li>
+            </ul>
+
+        </div>
