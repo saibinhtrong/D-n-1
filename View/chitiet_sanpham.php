65b2500eb80161558af15be14740aa9bfda565aa
thai <damthuanthai@gmail.com>
2023-11-20 10:35:35 +0700

    product detail

--- View/chitiet_sanpham.php
+++ View/chitiet_sanpham.php
@@ -19,9 +19,12 @@
                     </div>
                 </div>
                 <div class="carts">
+                    <button>
                     <div class="phienban">
+
                         <p>512GB</p>
                         <p>19.000.900</p>
+                    </button>
                     </div>
                     <div class="phienbans">
                         <p>256GB</p>
